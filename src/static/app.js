4a5f639d6b9e4344ea21350c073ea278eeba35fa
SigfridoGabriel <sigfrido_osorio2099@hotmail.com>
2026-01-18 00:50:35 +0000

    Commit #4

--- src/static/app.js
+++ src/static/app.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // Clear loading message
       activitiesList.innerHTML = "";
+      
 
       // Populate activities list
       Object.entries(activities).forEach(([name, details]) => {
